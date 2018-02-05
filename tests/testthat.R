99885e3089b2c8f42eaa34f4e2572a392b000388
Minoo <ashtiani.minoo@gmail.com>
2018-02-05 01:17:58 -0800

    change files
    Former-commit-id: bfb36b823eb5f25ec4d9a1f0bb5b8747964047fc [formerly 440026ca8619cf0c6cff9479f49c441a6235a929] [formerly bde0d3667eeee7629571bd1ea7bc44bbdeefa199 [formerly 8216197bc152c40ecde573563a49cf0dbe04bdbe]] [formerly 50725649d0d0fb22b0f841eef5d72916cf7670e3 [formerly 4025aa6cec33ca9cb0297b4d552a6a1d6df9e4b3] [formerly 6dbb3690755daf7f62fd622fef7327d69b0e7fac [formerly e1caf384935d0ee0dc1d82a854c78ec62b4878cf]]] [formerly 9c8562d016ef72c555bd5eafb7973bfde464b00f] [formerly 862da31ee3e52df57acc65dcbcbc84eebc0aea9b [formerly 862da31ee3e52df57acc65dcbcbc84eebc0aea9b [formerly 862da31ee3e52df57acc65dcbcbc84eebc0aea9b [formerly 862da31ee3e52df57acc65dcbcbc84eebc0aea9b [formerly c07b37c19e6700da5caba1fee504f4f72df4361e]]]]]
    Former-commit-id: 862da31ee3e52df57acc65dcbcbc84eebc0aea9b
    Former-commit-id: dc6bbebb45e37dc83d06aec2d0a66b70ca0e8c1e
    Former-commit-id: 2f66d2c97a2212341b228e511ba5aaf00807eb39 [formerly d74a4d4004362adc01c7b32b305d4e651eb885b6] [formerly d3ff504e77cd7700e18e961aa8da0d0422af0218 [formerly e27874172a6e279614baf9e14f622a4c0fa4a4ec]]
    Former-commit-id: ce7fe0aa0f475f12dab2ce6ddfcacf283e578beb [formerly 6fb367b343262d53338f0c5a1e670bae01620af9]
    Former-commit-id: 1bbc41090e1865efb2a3a2fdeff5a1a714fbd095

--- tests/testthat.R
+++ tests/testthat.R
@@ -2,3 +2,19 @@ library(testthat)
 library(IMMAN)
 
 test_check("IMMAN")
+
+data("H.sapiens")
+data("R.norvegicus")
+test_that("IMMAN reconstruct the interlog protein network", {
+  IMMAN(ProteinLists=list(as.character(H.sapiens$V1), as.character(R.norvegicus$V1)), fileNames=NULL, Species_IDs,
+        identityU = 30,
+        substitutionMatrix = "BLOSUM62",
+        gapOpening = -8,
+        gapExtension = -8,
+        NetworkShrinkage = FALSE,
+        coverage = 1,
+        BestHit = TRUE,
+        score_threshold = 400,
+        STRINGversion="10",
+        InputDirectory = getwd())
+})
